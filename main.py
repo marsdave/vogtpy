a4c8a073af85a5e882187a776701bf019bf292cf
marsdave <92921681+marsdave@users.noreply.github.com>
2021-10-25 18:10:38 +0200

    Update main.py
    Added ALS algorithm for baseline removal.

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ A number of Python functions that are useful for all kinds of coding situations.
 """
 
 import numpy as np
+from scipy import sparse
+
+def baseline_als(y, lam, p, niter=10):
+  L = len(y)
+  D = sparse.csc_matrix(np.diff(np.eye(L), 2))
+  w = np.ones(L)
+  for i in range(niter):
+    W = sparse.spdiags(w, 0, L, L)
+    Z = W + lam * D.dot(D.transpose())
+    z = sparse.linalg.spsolve(Z, w*y)
+    w = p * (y > z) + (1-p) * (y < z)
+  return z
 
 def round_to_significant(x, significant_digits=1):
     if hasattr(x, "__len__"):
